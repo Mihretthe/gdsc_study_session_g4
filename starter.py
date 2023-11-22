38a98b45fde4c9c6a68107e0a8a8bca80edce2df
Mihretthe <mihretekalgn@gmail.com>
2023-11-22 15:35:36 +0300

    Second Class

--- starter.py
+++ starter.py
@@ -1,1 +1,69 @@
-print("Hello World!")
+# print("Hello World!")
+# x = "Mihret"
+# print(x)
+# y = 123
+# x = 10
+# print(x + y)
+# print(type(x))
+# print(type(True))
+# print("abc"+"ab")
+# x = 31
+# x += x
+# print(x)
+# x = "MIHRET"
+# print(x[-2])
+
+# x = "to be or not to be that is the question"
+# print(x[-3:-1])
+# a = [1,2,3]
+# print(a[1])
+# b = a * 10
+# print(b)
+
+# a = 5
+# print(a // 2)
+# a = [1]
+# b = [a,a] #[[1],[1]]
+# a[0] = 3
+# # print(a,b)
+
+# a =[1]
+# b = [2, a] # [2,[1]]
+# c = [3, a, b] # [3, [1], [2,[1]]]
+# # print(b,c)
+
+
+# a = 3
+# b = 6
+# c = b / a # 2
+# a = b % c # 0 
+# b = a + b # 6
+# c = b + c # 8
+# print(c)
+
+# x = 4
+# y = x + 1 # 5
+# x = 2
+# print(x, y) # 2 3
+
+# x, y = 2, 6
+# x, y = y, x + 2
+# print(x, y) #
+
+
+# a, c = 2, 3
+# c, b = a, c + 1
+# print(a, b, c) #
+
+# print(2 > 3 or 3 > 2)
+# print(2 < 3 or not 3 > 2)
+# print(not 2 > 3 and 3 > 2)
+
+# True, False = False, True
+# print(True, False)
+# print(2 < 3)
+s =input( "Enter the name:")
+print(s)
+print(s[0])
+print(s[:5])
+print(s[3:6])
